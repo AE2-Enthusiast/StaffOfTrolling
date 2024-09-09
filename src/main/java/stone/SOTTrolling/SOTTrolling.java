30b9258812ab0572a784f54cb473301bc370da41
Raine Stone <samstone9001@gmail.com>
2024-09-09 13:34:29 -0400

    v1.1.0 bump!

--- src/main/java/stone/SOTTrolling/SOTTrolling.java
+++ src/main/java/stone/SOTTrolling/SOTTrolling.java
@@ -11,7 +11,7 @@ public class SOTTrolling
 {
     public static final String MODID = "staffoftrolling";
     public static final String NAME = "Staff Of Trolling";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
